7457102426013307adb16b7aa1ed1135c9d94bc2
Eduardo Santana <edusf0@hotmail.com>
2022-12-13 10:38:15 -0300

    atualizado

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const diffInMs = new Date(d2) - new Date(d1)
 const diffInDays = diffInMs / (1000 * 60 * 60 * 24)
 
 document.getElementById('dias').innerHTML = diffInDays.toFixed(0)
+
+function dimensaoTela() {
+  var heightTela = window.innerHeight
+  var widthTela = window.innerWidth
+
+  document.getElementById('pag_processos').style.minHeight = heightTela + 'px'
+}
